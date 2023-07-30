e664d6b835fb68f869d1ce838318f3b9ff195485
jaemin <leejm_1994@naver.com>
2023-07-31 08:52:19 +0900

    added

--- src/main/java/Member/auth/LogoutController.java
+++ src/main/java/Member/auth/LogoutController.java
@@ -1,7 +1,10 @@
 package Member.auth;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import Controller.SubController;
 import Domain.Common.Service.MemberService;
@@ -13,7 +16,15 @@ public class LogoutController implements SubController{
 	
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp) {
-		// TODO Auto-generated method stub
+		HttpSession session = req.getSession();
+        session.invalidate(); // 세션을 무효화하여 로그아웃 처리
+
+        try {
+            // main.do 이동 - Redirect
+            resp.sendRedirect(req.getContextPath() + "/Main.do");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 		
 	}
 
--- src/main/webapp/JS/pageRout.js
+++ src/main/webapp/JS/pageRout.js
@@ -6,6 +6,7 @@
 	let fullURL = null;
 	let shoppingBtn = document.getElementById("shopping");
 	const ed_btn = document.querySelector(".edit");
+	const login_btn = document.getElementById("login-button");
 	
 const logo_btn = document.getElementById("logo");
 logo_btn.addEventListener("click",function(){
@@ -15,16 +16,57 @@ logo_btn.addEventListener("click",function(){
 	
 	location.href = fullURL;
 });
+
+function handleLoginLogout() {
+  const roleValueInput = document.getElementById("roleValue");
+  const loginButton = document.getElementById("login-button");
+
+  // 숨겨진 input 필드에서 현재 ROLE 값을 가져옵니다.
+  let role = roleValueInput.value;
+
+  if (role === 'Role_Member' || role === 'Role_user') {
+    // 사용자가 로그인한 경우 로그아웃 처리를 위해 /Logout.do로 Ajax 요청을 보냅니다.
+    axios.post('/TeamProject2/logout.do')
+      .then(response => {
+        if (response.status === 200) {
+          // 세션 정보를 지웁니다.
+          roleValueInput.value = '';
+          loginButton.textContent = 'login'; // 로그인 아이콘으로 변경합니다.
+        }
+      })
+      .catch(error => {
+        console.error('Logout error:', error);
+      });
+  } else {
+    // 사용자가 로그인하지 않은 경우, 로그인 페이지로 이동하도록 구현하면 됩니다.
+    // 예시로 주석 처리하였습니다.
+    pagePath = "/member/auth/Login.jsp"
+	fullURL = baseURL + pagePath;
+	
+	location.href = fullURL;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (role === 'Role_Member' || role === 'Role_user') {
+    login_btn.textContent = 'logout';
+  } else {
+    login_btn.textContent = 'login';
+  }
+  login_btn.addEventListener('click', handleLoginLogout);
+});
+
+
   
-  // 로그인 버튼 클릭 이벤트 핸들러
-  document.getElementById("login-button").addEventListener("click", function() {
+/*  // 로그인 버튼 클릭 이벤트 핸들러
+  login_btn.addEventListener("click", function() {
     // 로그인 버튼 클릭 시 로그인 페이지로 이동
 	pagePath = "/member/auth/Login.jsp"
 	fullURL = baseURL + pagePath;
 	
 	location.href = fullURL;
   });
-  
+  */
 
     if (role === "Role_Member") {
         shoppingBtn.addEventListener("click", function() {
